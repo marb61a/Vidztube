4495afd604d2955aa739c9e5a30df56a5661b760
martin byrne <marb61a@gmail.com>
2019-02-12 16:37:44 +0000

    updated commonActions.js and added mastHeadContainer styles

--- assets/js/commonActions.js
+++ assets/js/commonActions.js
@@ -3,5 +3,12 @@ $(document).ready(function(){
         var main = $("#mainSectionContainer");
         var nav = $("#sideNavContainer");
         
+        if(main.hasClass("leftPadding")){
+            nav.hide();    
+        } else {
+            nav.show();
+        }
+        
+        main.toggleClass("leftPadding");
     });
 })
