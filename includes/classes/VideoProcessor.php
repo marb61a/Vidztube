83c90bc5239d777097e01bf66ff8d0462b16b7f7
martin byrne <marb61a@gmail.com>
2019-02-14 16:42:57 +0000

    Updated videoprocessor.php upload functionality

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -4,6 +4,18 @@
         private $sizeLimit = 500000000;
         private $allowedTypes = array("mp4", "flv", "webm", "mkv", "vob", "ogv", "ogg", "avi", "wmv", "mov", "mpeg", "mpg");
         
+        // UNCOMMENT OF THE FOLLOWING DEPENDING ON YOUR OS
+        // private $ffmpegPath = "ffmpeg/mac/regular-xampp/ffmpeg"; // *** MAC (USING REGULAR XAMPP) ***
+        // private $ffmpegPath = "ffmpeg/mac/xampp-VM/ffmpeg"; // *** MAC (USING XAMPP VM) ***
+        private $ffmpegPath = "ffmpeg/linux/ffmpeg"; // *** LINUX ***
+        // private $ffmpegPath = "ffmpeg/windows/ffmpeg.exe"; //  *** WINDOWS ***
+        
+        // ASLO UNCOMMENT ONE OF THESE DEPENDING ON YOUR OS
+        // private $ffprobePath = "ffmpeg/mac/regular-xampp/ffprobe"; // *** MAC (USING REGULAR XAMPP) ***
+        // private $ffprobePath = "ffmpeg/mac/xampp-VM/ffprobe"; // *** MAC (USING XAMPP VM) ***
+        private $ffprobePath = "ffmpeg/linux/ffprobe"; // *** LINUX ***
+        // private $ffprobePath = "ffmpeg/windows/ffprobe.exe"; //  *** WINDOWS ***
+        
         public function __construct($con) {
             $this->con = $con;
         }
@@ -20,6 +32,24 @@
             if(!$isValidData) {
                 return false;
             }
+            
+            if(move_uploaded_file($videoData["tmp_name"], $tempFilePath)) {
+                $finalFilePath = $targetDir . uniqid() . ".mp4";
+                
+                if(!$this->insertVideoData($videoUploadData, $finalFilePath)) {
+                    echo "Insert query failed\n";
+                    return false;
+                }
+                
+                return true;
+            }
+        }
+        
+        private function isValidSize($data) {
+            return $data["size"] <= $this->sizeLimit;
+        }
+        
+        private function insertVideoData($uploadData, $filePath) {
             
         }
     }
