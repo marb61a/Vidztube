41a01a98831a03f02a3aa8e7f7288ab31afbf6bf
martin byrne <marb61a@gmail.com>
2019-02-18 16:44:00 +0000

    Updated signUp.php form

--- signUp.php
+++ signUp.php
@@ -1,5 +1,36 @@
 <?php
-
+    require_once("includes/config.php"); 
+    require_once("includes/classes/Account.php");
+    require_once("includes/classes/Constants.php"); 
+    require_once("includes/classes/FormSanitizer.php"); 
+    
+    $account = new Account($con);
+    
+    if(isset($_POST["submitButton"])){
+        $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
+        $lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
+        
+        $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+        
+        $email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
+        $email2 = FormSanitizer::sanitizeFormEmail($_POST["email2"]);
+    
+        $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+        $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
+        
+        $wasSuccessful = $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+        
+        if($wasSuccessful) {
+            $_SESSION["userLoggedIn"] = $username;
+            header("Location: index.php");
+        }
+    }
+    
+    function getInputName($name) {
+        if(isset($_POST[$name])) {
+            echo $_POST[$name];
+        }
+    }
 ?>
 
 <!DOCTYPE html>
