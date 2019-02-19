e7f6ad36e74242783c2dce4039e48c5f33179176
martin byrne <marb61a@gmail.com>
2019-02-19 16:18:02 +0000

    Updated account.php methods to validate firstname, lastname and username

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -41,6 +41,33 @@
             return $query->execute();
         }
         
+        private function validateFirstName($fn) {
+            if(strlen($fn) > 25 || strlen($fn) < 2) {
+                array_push($this->errorArray, Constants::$firstNameCharacters);
+            }
+        }
+        
+        private function validateLastName($ln) {
+            if(strlen($ln) > 25 || strlen($ln) < 2) {
+                array_push($this->errorArray, Constants::$lastNameCharacters);
+            }
+        }
+        
+        private function validateUsername($un) {
+            if(strlen($un) > 25 || strlen($un) < 5) {
+                array_push($this->errorArray, Constants::$usernameCharacters);
+                return;
+            }
+            
+            $query = $this->con->prepare("SELECT username FROM users WHERE username=:un");
+            $query->bindParam(":un", $un);
+            $query->execute();
+            
+            if($query->rowCount() != 0) {
+                array_push($this->errorArray, Constants::$usernameTaken);
+            }
+        }
+        
         public function getError($error) {
             if(in_array($error, $this->errorArray)) {
                 return "<span class='errorMessage'>$error</span>";
