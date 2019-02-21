80a9a3b02914eaa035c5173298fde7fba921ccb4
martin byrne <marb61a@gmail.com>
2019-02-21 16:09:57 +0000

    Updated signin form

--- signIn.php
+++ signIn.php
@@ -3,7 +3,26 @@
     require_once("includes/classes/Account.php");
     require_once("includes/classes/Constants.php"); 
     require_once("includes/classes/FormSanitizer.php");
-
+    
+    $account = new Account($con);
+    
+    if(isset($_POST["submitButton"])) {
+        $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+        $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+        
+        $wasSuccessful = $account->login($username, $password);
+        
+        if($was) {
+            $_SESSION["userLoggedIn"] = $username;
+            header("Location: index.php");
+        }
+    }
+    
+    function getInputValue($name) {
+        if(isset($_POST[$name])) {
+            echo $_POST[$name];
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -28,6 +47,17 @@
                     <h3>Sign In</h3>
                     <span>to continue to VidzTube</span>
                 </div>
+                <!-- Login Form -->
+                <div class="loginForm">
+                    <form action="signIn.php" method="POST">
+                        <?php echo $account->getError(Constants::$loginFailed); ?>
+                        <input type="text" name="username" placeholder="Username" value="<?php getInputValue('username'); ?>" required autocomplete="off">
+                        <input type="password" name="password" placeholder="Password" required>
+                        <input type="submit" name="submitButton" value="SUBMIT">
+                    </form>
+                </div>
+                
+                <a class="signInMessage" href="signUp.php">Need an account? Sign up here!</a>
             </div>
         </div>
     </body>
