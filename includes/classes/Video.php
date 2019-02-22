22d86480c881e4f3060e157b418c2fb7cdb0b5fb
martin byrne <marb61a@gmail.com>
2019-02-22 16:38:45 +0000

    Updated Video.php

--- includes/classes/Video.php
+++ includes/classes/Video.php
@@ -12,5 +12,41 @@
             return $this->sqlData["id"];
         }
         
+        public function getUploadedBy() {
+            return $this->sqlData["uploadedBy"];
+        }
+    
+        public function getTitle() {
+            return $this->sqlData["title"];
+        }
+    
+        public function getDescription() {
+            return $this->sqlData["description"];
+        }
+    
+        public function getPrivacy() {
+            return $this->sqlData["privacy"];
+        }
+    
+        public function getFilePath() {
+            return $this->sqlData["filePath"];
+        }
+    
+        public function getCategory() {
+            return $this->sqlData["category"];
+        }
+    
+        public function getUploadDate() {
+            return $this->sqlData["uploadDate"];
+        }
+    
+        public function getViews() {
+            return $this->sqlData["views"];
+        }
+    
+        public function getDuration() {
+            return $this->sqlData["duration"];
+        }
+
     }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php require_once("includes/header.php"); ?>
 
-    Hi Everybody
+<?php
+    if(isset($_SESSION["isLoggedIn"])) {
+        echo "user is logged in as " . $userLoggedInObj->getName();;
+    } else {
+        echo "User not logged in";
+    }
+?>
 
 <?php require_once("includes/footer.php"); ?>
                     
