86ffa719018480812927086d6aa65eeac4273187
martin byrne <marb61a@gmail.com>
2019-02-25 16:47:08 +0000

    updated VideoPlayerAction.js file with dislikeVideo function

--- assets/js/videoPlayerActions.js
+++ assets/js/videoPlayerActions.js
@@ -5,7 +5,7 @@ function likeVideo(button, videoId) {
             var dislikeButton = $(button).siblings(".dislikeButton");
             
             likeButton.addClass("active");
-            likeButton.removeClass("active");
+            dislikeButton.removeClass("active");
             
             var result = JSON.parse(data);
             updateLikesValue(likeButton.find(".text"), result.likes);
@@ -20,6 +20,30 @@ function likeVideo(button, videoId) {
             
             dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
         });
+}
+
+function dislikeVideo(button, videoId) {
+    $.post("ajax/dislikeVideo.php", {videoId: videoId}
+        .done(function(data) {
+            var dislikeButton = $(button);
+            var likeButton = $(button).siblings(".likeButton");
+            
+            dislikeButton.addClass("active");
+            likeButton.removeClass("active");
+            
+            var result = JSON.parse(data);
+            updateLikesValue(likeButton.find(".text"), result.likes);
+            updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+            
+            if(result.dislikes < 0) {
+                dislikeButton.removeClass("active");
+                dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+            } else {
+                dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down-active.png")
+            }
+            
+            likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+        })
 }
 
 function updateLikesValue(element, num) {
