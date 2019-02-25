9161c4c77b49e3ae841ec72a4d5f1e79ab054013
martin byrne <marb61a@gmail.com>
2019-02-25 16:25:56 +0000

    updated VideoInfoControls class file

--- includes/classes/VideoInfoControls.php
+++ includes/classes/VideoInfoControls.php
@@ -18,5 +18,35 @@
                 $dislikeButton
             </div>";
         }
+        
+        private function createLikeButton() {
+            $text = $this->video->getLikes();
+            $videoId = $this->video->getId();
+            $action = "likeVideo(this, $videoId)";
+            $class = "likeButton";
+    
+            $imageSrc = "assets/images/icons/thumb-up.png";
+    
+            if($this->video->wasLikedBy()) {
+                $imageSrc = "assets/images/icons/thumb-up-active.png";
+            }
+    
+            return ButtonProvider::createButton($text, $imageSrc, $action, $class);
+        }
+        
+        private function createDislikeButton() {
+            $text = $this->video->getDislikes();
+            $videoId = $this->video->getId();
+            $action = "dislikeVideo(this, $videoId)";
+            $class = "dislikeButton";
+            
+            $imageSrc = "assets/images/icons/thumb-down.png";
+    
+            if($this->video->wasDislikedBy()) {
+                $imageSrc = "assets/images/icons/thumb-down-active.png";
+            }
+    
+            return ButtonProvider::createButton($text, $imageSrc, $action, $class);
+        }
     } 
 ?>
