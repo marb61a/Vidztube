d33137066a15509f5c9ec2ff49bf60fd89198169
martin byrne <marb61a@gmail.com>
2019-03-06 15:31:38 +0000

    updated comment section

--- includes/classes/CommentSection.php
+++ includes/classes/CommentSection.php
@@ -6,6 +6,16 @@
             $this->con = $con;
             $this->video = $video;
             $this->userLoggedInObj = $userLoggedInObj;
+            
+            $profileButton = ButtonProvider::createUserProfileButton($this->con, $postedBy);
+            $commentAction = "postComment(this, \"$postedBy\", $videoId, null, \"comments\")";
+            $commentButton = ButtonProvider::createButton("COMMENT", null, $commentAction, "postComment");
+            
+            $comments = $this->video->getComments();
+            $commentItems = "";
+            foreach($comments as $comment) {
+                $commentItems .= $comment->create();
+            }
         }
     
         public function create() {
@@ -19,7 +29,17 @@
             
             return "<div class='commentSection'>
                 <div class='header'>
+                    <span class='commentCount'>$numComments Comments</span>
+
+                    <div class='commentForm'>
+                        $profileButton
+                            <textarea class='commentBodyClass' placeholder='Add a public comment'></textarea>
+                        $commentButton
+                    </div>
+                </div>
                 
+                <div class='comments'>
+                    $commentItems
                 </div>
             </div>";
         }
