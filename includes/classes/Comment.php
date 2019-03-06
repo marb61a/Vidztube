ac44445bf1879ae30cc48088da304702ae1fc4c8
martin byrne <marb61a@gmail.com>
2019-03-06 15:48:59 +0000

    updated Comment.php time-elapsed function

--- includes/classes/Comment.php
+++ includes/classes/Comment.php
@@ -31,10 +31,20 @@
             $commentControlsObj = new CommentControls($this->con, $this, $this->userLoggedInObj);
             $commentControls = $commentControlsObj->create();
             
+            $numResponses = $this->getNumberOfReplies();
+            
+            if($numResponses > 0) {
+                
+            } else {
+                
+            }
+            
             return "<div class='itemContainer'>
                 <div class='comment'>
                     $profileButton
+                    <div class=''mainContainer>
                     
+                    </div>
                 </div>
             </div>";
         }
@@ -42,8 +52,31 @@
         public function time_elapsed_string($datetime, $full = false) {
             $now = new DateTime;
             $ago = new DateTime($datetime);
+            
             $diff = $now->diff($ago);
-        
+            $diff->w = floor($diff->d / 7);
+            $diff->d -= $diff->w * 7;
+            
+            $string = array(
+                "y" => "year",
+                'm' => 'month',
+                'w' => 'week',
+                'd' => 'day',
+                'h' => 'hour',
+                'i' => 'minute',
+                's' => 'second',
+            );
+            
+            foreach($string as $k => &$v) {
+                if($diff->$k) {
+                    $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+                } else {
+                    unset($string[$k]);
+                }
+            }
+            
+            if (!$full) $string = array_slice($string, 0, 1);
+            return $string ? implode(', ', $string) . ' ago' : 'just now';
         } 
         
         public function getId() {
