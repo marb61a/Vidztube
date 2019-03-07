b5a99be36df47f7a83cd74e9a02d09c7547b6c0d
martin byrne <marb61a@gmail.com>
2019-03-07 15:26:05 +0000

    added likeComment function to commentActions.js

--- assets/js/commentActions.js
+++ assets/js/commentActions.js
@@ -21,3 +21,33 @@ function postComment(button, postedBy, videoId, replyTo, containerClass) {
         alert("You can't post an empty comment");
     }
 }
+
+function toggleReply(button) {
+    var parent = $(button).closest(".itemContainer");
+    var commentForm = parent.find(".commentForm").first();
+
+    commentForm.toggleClass("hidden");
+}
+
+function likeComment(commentId, button, videoId) {
+     $.post("ajax/likeComment.php", { commentId: commentId, videoId: videoId })
+        .done(function(numToChange) {
+            var likeButton = $(button);
+            var dislikeButton = $(button).siblings(".dislikeButton");
+            
+            likeButton.addClass("active");
+            dislikeButton.removeClass("active");
+            
+            var likesCount = $(button).siblings(".likesCount");
+            updateLikesValue(likesCount, numToChange);
+            
+            if(numToChange < 0) {
+                likeButton.removeClass("active");
+                likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+            } else {
+                likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up-active.png");
+            }
+            
+            dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+        });
+}
