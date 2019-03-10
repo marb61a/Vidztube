13717e8ec6148dbd5daa023349a3c1935b031366
martin byrne <marb61a@gmail.com>
2019-03-10 18:45:09 +0000

    updated CommentControls.php

--- includes/classes/CommentControls.php
+++ includes/classes/CommentControls.php
@@ -27,8 +27,33 @@
             return ButtonProvider::createButton($text, null, $action, null);
         }
         
+        private function createLikesCount() {
+            $text = $this->comment->getLikes();
+    
+            if($text == 0) $text = "";
+    
+            return "<span class='likesCount'>$text</span>";
+        }
+    
         private function createReplySection() {
+            $postedBy = $this->userLoggedInObj->getUsername();
+            $videoId = $this->comment->getVideoId();
+            $commentId = $this->comment->getId();
+    
+            $profileButton = ButtonProvider::createUserProfileButton($this->con, $postedBy);
             
+            $cancelButtonAction = "toggleReply(this)";
+            $cancelButton = ButtonProvider::createButton("Cancel", null, $cancelButtonAction, "cancelComment");
+    
+            $postButtonAction = "postComment(this, \"$postedBy\", $videoId, $commentId, \"repliesSection\")";
+            $postButton = ButtonProvider::createButton("Reply", null, $postButtonAction, "postComment");
+    
+            return "<div class='commentForm hidden'>
+                $profileButton
+                <textarea class='commentBodyClass' placeholder='Add a public comment'></textarea>
+                $cancelButton
+                $postButton
+            </div>";
         }
     }
 ?>
