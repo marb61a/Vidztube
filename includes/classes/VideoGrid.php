eed7da0e004c7e4acf8d34337515e194a13663e7
martin byrne <marb61a@gmail.com>
2019-03-13 16:01:16 +0000

    updated videoGrid.php with create and generateItems methods

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -9,8 +9,38 @@
             $this->userLoggedInObj = $userLoggedInObj;
         }
         
-        public function create() {
+        public function create($videos, $title, $showFilter) {
+            if($videos == null) {
+                $gridItems = $this->generateItems();
+            } else {
+                $gridItems = $this->generateItemsFromVideos($videos);
+            }
             
+            $header = "";
+            
+            if($title != null) {
+                $header = $this->createGridHeader($title, $showFilter);
+            }
+            
+            return "$header
+                <div class='$this->gridClass'>
+                    $gridItems
+                </div>
+            ";
+        }
+        
+        public function generateItems() {
+            $query = $this->con->prepare("SELECT * FROM videos ORDER BY RAND() LIMIT 15");
+            $query->execute();
+            
+            $elementsHtml = "";
+            while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $video = new Video($this->con, $row, $this->userLoggedInObj);
+                $item = new VideoGridItem($video, $this->largeMode);
+                $elementsHtml .= $item->create();
+            }
+    
+            return $elementsHtml;
         }
     }
 ?>
