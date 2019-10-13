82f4dfa748f916bc49f02baa9dafb7021ee16908
Martin Byrne <marb61a@gmail.com>
2019-10-13 10:52:22 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,14 @@
 <?php
     // Turn on output buffering
     ob_start();
+    session_start();
     
     // Timezone
     date_default_timezone_set("Europe/Dublin");
     
     // DB Connection
     try{
-        $con = new PDO();
+        $con = new PDO("mysql:host=127.0.0.1:4848;dbname=vidztube", "root", "");
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
